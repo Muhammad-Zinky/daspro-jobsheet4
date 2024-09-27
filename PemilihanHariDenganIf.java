1029db3421ffd9ddd24c79f6af59d21a71d53f9e
mohammad zinky <mohammadzinky@gmail.com>
2024-09-27 15:28:52 +0700

    hooh

--- PemilihanHariDenganIf.java
+++ PemilihanHariDenganIf.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PemilihanHari10 {
+public class PemilihanHariDenganIf {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -10,15 +10,15 @@ public class PemilihanHari10 {
         dayname = sc.nextLine();
 
         switch (dayname.toLowerCase()) {
-            case "monday":
-            case "tuesday":
-            case "wednesday":
-            case "thursday":
-            case "friday":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
                 daytype = "weekday";
                 break;
-            case "saturday":
-            case "sunday":
+            case "6":
+            case "7":
             daytype = "weekend";
             break;
             default:
